a88fceee26136a6dfdee960094f5d720718b9def
jbrad88 <jodybradley@gmail.com>
2020-10-17 16:08:11 +0100

    Adding first draft of sqrt2.py

---
+++ sqrt2.py
@@ -0,0 +1,10 @@
+# This program calculates and prints to the screen the square root of 2 to 100 decimal places.
+
+x = float(2**(1/2))
+
+print(x)
+
+
+# References:
+# 1. https://www.tutorialspoint.com/How-to-perform-square-root-without-using-math-module-in-Python 
+#
